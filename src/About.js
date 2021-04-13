dff54bb2bd424a94d6c0253bd444ae54d36c8d35
Aditya Gupta <adityagupta.1921997@gmail.com>
2021-04-13 22:40:56 +0530

    First Commit

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return(
+        <div>
+            <field>Inside About Page</field>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return(
+        <div>
+            <heading>Inside Home Page</heading>
+        </div>
+    );
+}
+
+export default Home;
